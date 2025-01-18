ea126e704262a580b83a4493d2b1fb026a842a7a
Muhammad Rizky Ardiansah <rizkyardiansah.dev@gmail.com>
2025-01-18 20:56:04 +0700

    create connection route

--- backend/emails/emailHandlers.js
+++ backend/emails/emailHandlers.js
@@ -39,3 +39,17 @@ export const sendCommentNotificationEmail = async (
         throw error
     }
 }
+
+export const sendConnectionAcceptedEmail = async (senderEmail, senderName, recipientName, profileUrl) => {
+    const recipient = [{ email: senderEmail }];
+
+    try {
+        const response = await mailtrapClient.send({
+            from: sender,
+            to: recipient,
+            subject: `${recipientName} accepted your connection request`,
+            html: createConnectionAcceptedEmailTemplate(senderName, recipientName, profileUrl),
+            category: "connection_accepted",
+        });
+    } catch (error) { }
+};
